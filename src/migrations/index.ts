e50ffb8908e5ca7a8a3952c6978c6c83d9792d1b
samuel-eric <samueltan086@gmail.com>
2025-04-14 09:20:15 +0700

    Added new migration for access control

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250414_013733 from './20250414_013733';
+import * as migration_20250414_022002 from './20250414_022002';
 
 export const migrations = [
   {
     up: migration_20250414_013733.up,
     down: migration_20250414_013733.down,
-    name: '20250414_013733'
+    name: '20250414_013733',
+  },
+  {
+    up: migration_20250414_022002.up,
+    down: migration_20250414_022002.down,
+    name: '20250414_022002'
   },
 ];
