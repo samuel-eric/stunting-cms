3287ef7e45762be21c67eaf949cd28c9b50fdaad
samuel-eric <samueltan086@gmail.com>
2025-04-30 14:58:37 +0700

    Generated importmap

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -159,7 +159,6 @@ export interface User {
 export interface Media {
   id: number;
   alt: string;
-  prefix?: string | null;
   updatedAt: string;
   createdAt: string;
   url?: string | null;
@@ -337,7 +336,6 @@ export interface UsersSelect<T extends boolean = true> {
  */
 export interface MediaSelect<T extends boolean = true> {
   alt?: T;
-  prefix?: T;
   updatedAt?: T;
   createdAt?: T;
   url?: T;
